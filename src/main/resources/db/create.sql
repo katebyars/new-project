3b9cfa8458e36b57b8555dcbeb423b4271895818
Kate Byars <katebyars5@gmail.com>
2017-08-25 10:04:52 -0700

    add all tables to create.sql.

---
+++ src/main/resources/db/create.sql
@@ -0,0 +1,65 @@
+SET MODE PostgreSQL;
+
+CREATE TABLE IF NOT EXISTS languages(
+ id int PRIMARY KEY auto_increment,
+ name VARCHAR,
+);
+
+
+CREATE TABLE IF NOT EXISTS locations (
+ id int PRIMARY KEY auto_increment,
+ name VARCHAR,
+ city VARCHAR,
+ region VARCHAR,
+ address VARCHAR,
+);
+
+
+CREATE TABLE IF NOT EXISTS phrases (
+ id int PRIMARY KEY auto_increment,
+ phrase VARCHAR,
+ definition VARCHAR,
+ languageid int,
+ wordId int
+);
+
+
+CREATE TABLE IF NOT EXISTS residences(
+ id int PRIMARY KEY auto_increment,
+ residenceowner VARCHAR,
+ foodiate VARCHAR,
+ description VARCHAR,
+ locationid int
+);
+
+
+CREATE TABLE IF NOT EXISTS restaurants (
+ id int PRIMARY KEY auto_increment,
+ name VARCHAR,
+ foodiate VARCHAR,
+ address VARCHAR,
+ description VARCHAR,
+ locationid int
+ );
+
+
+CREATE TABLE IF NOT EXISTS sights (
+ id int PRIMARY KEY auto_increment,
+ name VARCHAR,
+ address VARCHAR,
+ description VARCHAR,
+ locationid int
+ );
+
+
+CREATE TABLE IF NOT EXISTS words (
+ id int PRIMARY KEY auto_increment,
+ word VARCHAR,
+ definition VARCHAR,
+ photo VARCHAR,
+ languageid int
+ );
+
+
+
+
